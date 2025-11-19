1cf2ccb03fe18ec97f28d5d11b3b6a7017a4c54f
Joe Lezala <joe.lezala@torstonetech.com>
2025-11-19 11:04:26 +0000

    rename build files

--- build.bat
+++ build.bat
@@ -1,7 +1,9 @@
-echo WAITING FOR PDB > lock.tmp
+echo WAITING FOR PDB > ./build/lock.tmp
 
 odin build game -build-mode:dll -out:./build/game.dll -debug -vet
 
+cd build
 del lock.tmp
+cd ..
 
 odin build platform -out:./build/run.exe -debug -vet
